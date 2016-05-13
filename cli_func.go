b8d335a387c7fd2a1a56963a59974d4b594cc78f
Qian Chen <elgs1980@hotmail.com>
2016-05-13 12:12:11 -0700

    Update.

---
+++ cli_func.go
@@ -0,0 +1,74 @@
+// cli_func
+package main
+
+import (
+	"encoding/json"
+)
+
+func processCliCommand(message []byte) (string, error) {
+	cliCommand := &Command{}
+	json.Unmarshal(message, cliCommand)
+	switch cliCommand.Type {
+	case "CLI_DN_LIST":
+		return masterData.ListDataNodes(cliCommand.Data), nil
+	case "CLI_DN_ADD":
+		dataNode := &DataNode{}
+		err := json.Unmarshal([]byte(cliCommand.Data), dataNode)
+		if err != nil {
+			return "", err
+		}
+		err = masterData.AddDataNode(dataNode)
+		if err != nil {
+			return "", err
+		}
+	case "CLI_DN_UPDATE":
+		dataNode := &DataNode{}
+		err := json.Unmarshal([]byte(cliCommand.Data), dataNode)
+		if err != nil {
+			return "", err
+		}
+		err = masterData.UpdateDataNode(dataNode)
+		if err != nil {
+			return "", err
+		}
+	case "CLI_DN_REMOVE":
+		err := masterData.RemoveDataNode(cliCommand.Data)
+		if err != nil {
+			return "", err
+		}
+	case "CLI_APP_LIST":
+		return masterData.ListApps(cliCommand.Data), nil
+	case "CLI_APP_ADD":
+		app := &App{}
+		err := json.Unmarshal([]byte(cliCommand.Data), app)
+		if err != nil {
+			return "", err
+		}
+		err = masterData.AddApp(app)
+		if err != nil {
+			return "", err
+		}
+	case "CLI_APP_UPDATE":
+		app := &App{}
+		err := json.Unmarshal([]byte(cliCommand.Data), app)
+		if err != nil {
+			return "", err
+		}
+		err = masterData.UpdateApp(app)
+		if err != nil {
+			return "", err
+		}
+	case "CLI_APP_REMOVE":
+		err := masterData.RemoveApp(cliCommand.Data)
+		if err != nil {
+			return "", err
+		}
+	case "CLI_SHOW_MASTER":
+		masterDataBytes, err := json.Marshal(masterData)
+		if err != nil {
+			return "", err
+		}
+		return string(masterDataBytes), nil
+	}
+	return "", nil
+}
