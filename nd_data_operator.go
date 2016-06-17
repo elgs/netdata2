384655faad6cada5f7ba7c9f9006e85e8483c194
Qian Chen <elgs1980@hotmail.com>
2016-06-17 09:46:34 -0700

    Update.

---
+++ nd_data_operator.go
@@ -0,0 +1,358 @@
+// nd_data_operator
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/elgs/gorest2"
+	"github.com/elgs/gosplitargs"
+	"github.com/elgs/gosqljson"
+)
+
+type NdDataOperator struct {
+	*gorest2.MySqlDataOperator
+}
+
+func NewDbo(ds, dbType string) gorest2.DataOperator {
+	return &NdDataOperator{
+		MySqlDataOperator: &gorest2.MySqlDataOperator{
+			Ds:     ds,
+			DbType: dbType,
+		},
+		//		QueryRegistry: make(map[string]map[string]string),
+	}
+}
+
+func loadQuery(projectId, queryName string) (map[string]string, error) {
+	key := fmt.Sprint("query:", projectId, ":", queryName)
+	queryMap := gorest2.RedisLocal.HGetAllMap(key).Val()
+	if len(queryMap) > 0 {
+		return queryMap, nil
+	}
+
+	defaultDbo := gorest2.GetDbo("default")
+	defaultDb, err := defaultDbo.GetConn()
+	if err != nil {
+		return nil, err
+	}
+	queryData, err := gosqljson.QueryDbToMap(defaultDb, "upper",
+		"SELECT * FROM query WHERE PROJECT_ID=? AND NAME=?", projectId, queryName)
+	if err != nil {
+		return nil, err
+	}
+	if len(queryData) == 0 {
+		return nil, errors.New("Query not found.")
+	}
+
+	err = gorest2.RedisMaster.HMSet(key, "name", queryData[0]["NAME"], "script", queryData[0]["SCRIPT"]).Err()
+	return queryData[0], nil
+}
+
+func (this *NdDataOperator) QueryMap(tableId string, params []interface{}, queryParams []string, context map[string]interface{}) ([]map[string]string, error) {
+	projectId := context["app_id"].(string)
+	query, err := loadQuery(projectId, tableId)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := make([]map[string]string, 0)
+
+	script := query["script"]
+
+	count, err := gosplitargs.CountSeparators(script, "\\?")
+	if err != nil {
+		return ret, err
+	}
+	if count > len(params) {
+		return nil, errors.New(fmt.Sprintln("Incorrect param count. Expected: ", count, " actual: ", len(params)))
+	}
+
+	for i, v := range queryParams {
+		script = strings.Replace(script, fmt.Sprint("$", i), v, -1)
+	}
+
+	db, err := this.GetConn()
+	if err != nil {
+		return ret, err
+	}
+
+	globalDataInterceptors, globalSortedKeys := gorest2.GetGlobalDataInterceptors()
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		ctn, err := globalDataInterceptor.BeforeQueryMap(tableId, script, &params, db, context)
+		if !ctn {
+			return ret, err
+		}
+	}
+	dataInterceptors, sortedKeys := gorest2.GetDataInterceptors(tableId)
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			ctn, err := dataInterceptor.BeforeQueryMap(tableId, script, &params, db, context)
+			if !ctn {
+				return ret, err
+			}
+		}
+	}
+
+	if clientIp, ok := context["client_ip"].(string); ok {
+		script = strings.Replace(script, "__ip__", clientIp, -1)
+	}
+	if tokenUserId, ok := context["token_user_id"].(string); ok {
+		script = strings.Replace(script, "__token_user_id__", tokenUserId, -1)
+	}
+	if tokenUserCode, ok := context["token_user_code"].(string); ok {
+		script = strings.Replace(script, "__token_user_code__", tokenUserCode, -1)
+	}
+	if loginUserId, ok := context["user_id"].(string); ok {
+		script = strings.Replace(script, "__login_user_id__", loginUserId, -1)
+	}
+	if loginUserCode, ok := context["email"].(string); ok {
+		script = strings.Replace(script, "__login_user_code__", loginUserCode, -1)
+	}
+
+	c := context["case"].(string)
+	m, err := gosqljson.QueryDbToMap(db, c, script, params[:count]...)
+	if err != nil {
+		fmt.Println(err)
+		return ret, err
+	}
+
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			dataInterceptor.AfterQueryMap(tableId, script, &params, db, context, &m)
+		}
+	}
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		globalDataInterceptor.AfterQueryMap(tableId, script, &params, db, context, &m)
+	}
+
+	return m, err
+}
+func (this *NdDataOperator) QueryArray(tableId string, params []interface{}, queryParams []string, context map[string]interface{}) ([]string, [][]string, error) {
+	projectId := context["app_id"].(string)
+	query, err := loadQuery(projectId, tableId)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	script := query["script"]
+	count, err := gosplitargs.CountSeparators(script, "\\?")
+	if err != nil {
+		return nil, nil, err
+	}
+	if count > len(params) {
+		return nil, nil, errors.New(fmt.Sprintln("Incorrect param count. Expected: ", count, " actual: ", len(params)))
+	}
+
+	for i, v := range queryParams {
+		script = strings.Replace(script, fmt.Sprint("$", i), v, -1)
+	}
+
+	db, err := this.GetConn()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	globalDataInterceptors, globalSortedKeys := gorest2.GetGlobalDataInterceptors()
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		ctn, err := globalDataInterceptor.BeforeQueryArray(tableId, script, &params, db, context)
+		if !ctn {
+			return nil, nil, err
+		}
+	}
+	dataInterceptors, sortedKeys := gorest2.GetDataInterceptors(tableId)
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			ctn, err := dataInterceptor.BeforeQueryArray(tableId, script, &params, db, context)
+			if !ctn {
+				return nil, nil, err
+			}
+		}
+	}
+
+	if clientIp, ok := context["client_ip"].(string); ok {
+		script = strings.Replace(script, "__ip__", clientIp, -1)
+	}
+	if tokenUserId, ok := context["token_user_id"].(string); ok {
+		script = strings.Replace(script, "__token_user_id__", tokenUserId, -1)
+	}
+	if tokenUserCode, ok := context["token_user_code"].(string); ok {
+		script = strings.Replace(script, "__token_user_code__", tokenUserCode, -1)
+	}
+	if loginUserId, ok := context["user_id"].(string); ok {
+		script = strings.Replace(script, "__login_user_id__", loginUserId, -1)
+	}
+	if loginUserCode, ok := context["email"].(string); ok {
+		script = strings.Replace(script, "__login_user_code__", loginUserCode, -1)
+	}
+
+	c := context["case"].(string)
+	h, a, err := gosqljson.QueryDbToArray(db, c, script, params[:count]...)
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil, err
+	}
+
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			dataInterceptor.AfterQueryArray(tableId, script, &params, db, context, &h, &a)
+		}
+	}
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		globalDataInterceptor.AfterQueryArray(tableId, script, &params, db, context, &h, &a)
+	}
+
+	return h, a, err
+}
+func (this *NdDataOperator) Exec(tableId string, params [][]interface{}, queryParams []string, context map[string]interface{}) ([][]int64, error) {
+	projectId := context["app_id"].(string)
+
+	query, err := loadQuery(projectId, tableId)
+	if err != nil {
+		return nil, err
+	}
+	scripts := query["script"]
+
+	db, err := this.GetConn()
+	if err != nil {
+		return nil, err
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	globalDataInterceptors, globalSortedKeys := gorest2.GetGlobalDataInterceptors()
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		ctn, err := globalDataInterceptor.BeforeExec(tableId, scripts, &params, queryParams, tx, context)
+		if !ctn {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+	dataInterceptors, sortedKeys := gorest2.GetDataInterceptors(tableId)
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			ctn, err := dataInterceptor.BeforeExec(tableId, scripts, &params, queryParams, tx, context)
+			if !ctn {
+				tx.Rollback()
+				return nil, err
+			}
+		}
+	}
+
+	replaceContext := buildReplaceContext(context)
+	rowsAffectedArray, err := batchExecuteTx(tx, nil, &scripts, queryParams, params, replaceContext)
+
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			err := dataInterceptor.AfterExec(tableId, scripts, &params, queryParams, tx, context, rowsAffectedArray)
+			if err != nil {
+				tx.Rollback()
+				return nil, err
+			}
+		}
+	}
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		err := globalDataInterceptor.AfterExec(tableId, scripts, &params, queryParams, tx, context, rowsAffectedArray)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
+	tx.Commit()
+
+	return rowsAffectedArray, err
+}
+
+/*
+func (this *NdDataOperator) ExecX(tableId string, params [][]interface{}, queryParams []string, context map[string]interface{}) ([]gorest2.NdResult, error) {
+	projectId := context["app_id"].(string)
+
+	query, err := loadQuery(projectId, tableId)
+	if err != nil {
+		return nil, err
+	}
+	scripts := query["script"]
+
+	db, err := this.GetConn()
+	if err != nil {
+		return nil, err
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	globalDataInterceptors, globalSortedKeys := gorest2.GetGlobalDataInterceptors()
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		ctn, err := globalDataInterceptor.BeforeExecX(tableId, scripts, &params, queryParams, tx, context)
+		if !ctn {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+	dataInterceptors, sortedKeys := gorest2.GetDataInterceptors(tableId)
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			ctn, err := dataInterceptor.BeforeExecX(tableId, scripts, &params, queryParams, tx, context)
+			if !ctn {
+				tx.Rollback()
+				return nil, err
+			}
+		}
+	}
+
+	replaceContext := buildReplaceContext(context)
+	rowsAffectedArray, err := batchExecuteTx(tx, nil, &scripts, queryParams, params, replaceContext)
+
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	for _, k := range sortedKeys {
+		dataInterceptor := dataInterceptors[k]
+		if dataInterceptor != nil {
+			err := dataInterceptor.AfterExecX(tableId, scripts, &params, queryParams, tx, context, rowsAffectedArray)
+			if err != nil {
+				tx.Rollback()
+				return nil, err
+			}
+		}
+	}
+	for _, k := range globalSortedKeys {
+		globalDataInterceptor := globalDataInterceptors[k]
+		err := globalDataInterceptor.AfterExecX(tableId, scripts, &params, queryParams, tx, context, rowsAffectedArray)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
+	tx.Commit()
+
+	return rowsAffectedArray, err
+}
+*/
---
+++ ndutils.go
@@ -0,0 +1,206 @@
+// ndutils
+package main
+
+import (
+	"crypto/tls"
+	"database/sql"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/elgs/gojq"
+	"github.com/elgs/gosplitargs"
+	"github.com/elgs/gosqljson"
+)
+
+func httpRequest(url string, method string, data string, maxReadLimit int64) ([]byte, int, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	req, err := http.NewRequest(method, url, strings.NewReader(data))
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+	if maxReadLimit >= 0 {
+		res.Body = &LimitedReadCloser{res.Body, maxReadLimit}
+	}
+
+	result, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+
+	defer res.Body.Close()
+	defer tr.CloseIdleConnections()
+
+	return result, res.StatusCode, err
+}
+
+func batchExecuteTx(tx *sql.Tx, db *sql.DB, script *string, scriptParams []string, params [][]interface{}, replaceContext map[string]string) ([][]int64, error) {
+
+	rowsAffectedArray := [][]int64{}
+
+	innerTrans := false
+	if tx == nil {
+		var err error
+		tx, err = db.Begin()
+		innerTrans = true
+		if err != nil {
+			return rowsAffectedArray, err
+		}
+	}
+
+	for i, v := range scriptParams {
+		*script = strings.Replace(*script, fmt.Sprint("$", i), v, -1)
+	}
+
+	for k, v := range replaceContext {
+		*script = strings.Replace(*script, k, v, -1)
+	}
+
+	scriptsArray, err := gosplitargs.SplitArgs(*script, ";", true)
+	if err != nil {
+		if innerTrans {
+			tx.Rollback()
+		}
+		return rowsAffectedArray, err
+	}
+	for _, params1 := range params {
+		totalCount := 0
+		rowsAffectedArray1 := []int64{}
+		for _, s := range scriptsArray {
+			sqlNormalize(&s)
+			if len(s) == 0 {
+				continue
+			}
+			count, err := gosplitargs.CountSeparators(s, "\\?")
+			if err != nil {
+				if innerTrans {
+					tx.Rollback()
+				}
+				return nil, err
+			}
+			if len(params1) < totalCount+count {
+				if innerTrans {
+					tx.Rollback()
+				}
+				return nil, errors.New(fmt.Sprintln("Incorrect param count. Expected: ", totalCount+count, " actual: ", len(params1)))
+			}
+			rowsAffected, err := gosqljson.ExecTx(tx, s, params1[totalCount:totalCount+count]...)
+			if err != nil {
+				if innerTrans {
+					tx.Rollback()
+				}
+				return nil, err
+			}
+			rowsAffectedArray1 = append(rowsAffectedArray1, rowsAffected)
+			totalCount += count
+		}
+		rowsAffectedArray = append(rowsAffectedArray, rowsAffectedArray1)
+	}
+
+	if innerTrans {
+		tx.Commit()
+	}
+
+	return rowsAffectedArray, nil
+}
+
+func buildReplaceContext(context map[string]interface{}) map[string]string {
+	replaceContext := map[string]string{}
+	if clientIp, ok := context["client_ip"].(string); ok {
+		replaceContext["__ip__"] = clientIp
+	}
+	if loginUserId, ok := context["user_id"].(string); ok {
+		replaceContext["__login_user_id__"] = loginUserId
+	}
+	if loginUserCode, ok := context["email"].(string); ok {
+		replaceContext["__login_user_code__"] = loginUserCode
+	}
+	if tokenUserId, ok := context["token_user_id"].(string); ok {
+		replaceContext["__token_user_id__"] = tokenUserId
+	}
+	if tokenUserCode, ok := context["token_user_code"].(string); ok {
+		replaceContext["__token_user_code__"] = tokenUserCode
+	}
+	return replaceContext
+}
+
+func buildParams(clientData string) ([]string, [][]interface{}, error) {
+	// assume the clientData is a json object with two arrays: query_params and params
+	parser, err := gojq.NewStringQuery(clientData)
+	if err != nil {
+		return nil, nil, err
+	}
+	qp, err := parser.QueryToArray("query_params")
+	if err != nil {
+		return nil, nil, err
+	}
+	queryParams, err := convertInterfaceArrayToStringArray(qp)
+	if err != nil {
+		return nil, nil, err
+	}
+	p, err := parser.Query("params")
+	if p1, ok := p.([]interface{}); ok {
+		params := [][]interface{}{}
+		for _, p2 := range p1 {
+			if param, ok := p2.([]interface{}); ok {
+				params = append(params, param)
+			} else {
+				return nil, nil, errors.New("Failed to build params.")
+			}
+		}
+		return queryParams, params, nil
+	}
+	return nil, nil, errors.New("Failed to build.")
+}
+
+func convertInterfaceArrayToStringArray(arrayOfInterfaces []interface{}) ([]string, error) {
+	ret := []string{}
+	for _, v := range arrayOfInterfaces {
+		if s, ok := v.(string); ok {
+			ret = append(ret, s)
+		} else {
+			return nil, errors.New("Failed to convert.")
+		}
+	}
+	return ret, nil
+}
+
+func sqlNormalize(sql *string) {
+	*sql = strings.TrimSpace(*sql)
+	var ret string
+	lines := strings.Split(*sql, "\n")
+	for _, line := range lines {
+		lineTrimmed := strings.TrimSpace(line)
+		if lineTrimmed != "" && !strings.HasPrefix(lineTrimmed, "-- ") {
+			ret += line + "\n"
+		}
+	}
+	*sql = ret
+}
+
+func isQuery(sql string) bool {
+	sqlUpper := strings.ToUpper(strings.TrimSpace(sql))
+	if strings.HasPrefix(sqlUpper, "SELECT") ||
+		strings.HasPrefix(sqlUpper, "SHOW") ||
+		strings.HasPrefix(sqlUpper, "DESCRIBE") ||
+		strings.HasPrefix(sqlUpper, "EXPLAIN") {
+		return true
+	}
+	return false
+}
